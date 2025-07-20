721d18223d78da420d8200580cd91c5ee12acae0
arpa2001 <arpajitofficial@gmail.com>
2025-07-20 18:25:51 +0530

    creating generic dto for kafka

--- src/main/java/com/arpajit/holidayplanner/dto/KafkaMessage.java
+++ src/main/java/com/arpajit/holidayplanner/dto/KafkaMessage.java
@@ -1,15 +1,15 @@
 package com.arpajit.holidayplanner.dto;
 
-public class ControllerProducer {
+public class KafkaMessage {
     private String requestType;
     private String sourceService;
     private String timestamp;
-    private Object payload;
+    private String payload;
 
-    public ControllerProducer(String requestType,
+    public KafkaMessage(String requestType,
                               String sourceService,
                               String timestamp,
-                              Object payload) {
+                              String payload) {
         this.requestType = requestType;
         this.sourceService = sourceService;
         this.timestamp = timestamp;
@@ -19,5 +19,5 @@ public class ControllerProducer {
     public String getRequestType() {return requestType;}
     public String getSourceService() {return sourceService;}
     public String getTimestamp() {return timestamp;}
-    public Object getPayload() {return payload;}
+    public String getPayload() {return payload;}
 }
