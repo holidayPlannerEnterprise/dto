29d0fe75dc0b3589a97b2ab565f01a7077257fcc
arpa2001 <arpajitofficial@gmail.com>
2025-08-02 20:57:31 +0530

    adding back payload

--- src/main/java/com/arpajit/holidayplanner/dto/KafkaMessage.java
+++ src/main/java/com/arpajit/holidayplanner/dto/KafkaMessage.java
@@ -8,6 +8,7 @@ public class KafkaMessage {
     private String requestType;
     private String sourceService;
     private String timestamp;
+    private String payload;
     private String status;
     private String statusResp;
 
@@ -16,12 +17,14 @@ public class KafkaMessage {
                         @JsonProperty("requestType") String requestType,
                         @JsonProperty("sourceService") String sourceService,
                         @JsonProperty("timestamp") String timestamp,
+                        @JsonProperty("payload") String payload,
                         @JsonProperty("status") String status,
                         @JsonProperty("statusResp") String statusResp) {
         this.traceId = traceId;
         this.requestType = requestType;
         this.sourceService = sourceService;
         this.timestamp = timestamp;
+        this.payload = payload;
         this.status = status;
         this.statusResp = statusResp;
     }
@@ -30,6 +33,7 @@ public class KafkaMessage {
     public void setRequestType(String requestType) {this.requestType = requestType;}
     public void setSourceService(String sourceService) {this.sourceService = sourceService;}
     public void setTimestamp(String timestamp) {this.timestamp = timestamp;}
+    public void setPayload(String payload) {this.payload = payload;}
     public void setStatus(String status) {this.status = status;}
     public void setStatusResp(String statusResp) {this.statusResp = statusResp;}
 
@@ -37,6 +41,7 @@ public class KafkaMessage {
     public String getRequestType() {return requestType;}
     public String getSourceService() {return sourceService;}
     public String getTimestamp() {return timestamp;}
+    public String getPayload() {return payload;}
     public String getStatus() {return status;}
     public String getStatusResp() {return statusResp;}
 }
